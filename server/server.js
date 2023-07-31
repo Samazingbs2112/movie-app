cdb9b147650f2780b75ac2322fb3ba8d92e3f97a
Samy <s.boudour@hotmail.com>
2023-07-31 19:52:42 +0100

    removed-node-modules

--- server/server.js
+++ server/server.js
@@ -1,7 +1,25 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
-const db = require('./db');
+//const db = require('./db');
+const router = require('./routes/usersRoutes');
 const SERVER_PORT = process.env.SERVER_PORT || 3001;
+app.use(express.json())
+
+
+const corsConfig = {
+    origin: 'http://localhost:5173',
+    credentials: true,
+};
+
+app.use(cors(corsConfig));
+app.use(express.json());
+
+app.use(router);
+
+
+
+
 
 
 
